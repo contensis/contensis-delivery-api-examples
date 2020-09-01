09927ca6df112a6721368309cd3dd29f27d18726
Alex Pop <a.pop@zengenti.com>
2020-09-01 15:50:17 +0100

    updated example

--- angular-test-app/src/app/app.component.ts
+++ angular-test-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ComponentFactoryResolver } from '@angular/core';
 import { Client } from 'contensis-delivery-api';
+import { ResponseContext } from 'contensis-core-api';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +13,25 @@ export class AppComponent implements OnInit {
   contensisProjectName = '';
   error = null;
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     const client = Client.create({
-      accessToken: 'xxxxx',
+      accessToken: 'xxxx',
       projectId: 'website',
-      rootUrl: 'https://cms-example.cloud.contensis.com'
+      rootUrl: 'https://cms-example.cloud.contensis.com',
+      defaultHeaders: { 'x-require-surrogate-key': 'true' },
+      responseHandler: {
+        ['*']: (response: Response, context: ResponseContext) => {
+          if (response.url.endsWith('/nodes/root'))
+            console.log('Repsonse:', response);
+          response.headers.forEach((value, key) => {
+            console.log('Response header:', key, ' -', value);
+          });
+
+          console.log('Response context:', context)
+        }
+      }
     });
+
     client.project.get().then(project => {
       this.contensisProjectName = project.name;
     },
@@ -25,6 +39,11 @@ export class AppComponent implements OnInit {
         this.error = error;
       }
     );
+
+    let result = await client.nodes.getRoot();
+    if (result) {
+      console.log('Root node:', result);
+    }
   }
 
 }
